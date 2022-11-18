caa4b28d2fd0753d055d44c842c34d55b4c3b388
Sergey Uritskiy <3073373@mail.ru>
2022-11-18 23:06:55 +0500

    First commit

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Сергей Урицкий on 18.11.2022.
+//
+
+import Foundation
+import UIKit
+
+class ProfileView: UIView {
+    
+    
+    @IBOutlet weak var foto: UIImageView!
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var birth: UILabel!
+    
+    @IBOutlet weak var location: UILabel!
+    
+    @IBOutlet weak var historySergey: UITextView!
+    
+}
---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Сергей Урицкий on 18.11.2022.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+}
